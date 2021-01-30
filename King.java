8d8de3411582dec73f10c154e49884f585b7b774
Owen <owenburland@live.ca>
2021-01-30 12:02:39 -0600

    added logic to king

--- King.java
+++ King.java
@@ -1,6 +1,8 @@
 import java.awt.Toolkit;
 
 public class King extends Piece {
+
+	boolean canMove;
 	
 	public King(int x, int y, boolean color, Board brd) {
 		super(x,y,color,brd);
@@ -11,5 +13,13 @@ public class King extends Piece {
 			
 	}
  
+	public boolean move(int newx, int newy){
+		canMove = false;
+		if(brd.getpiece(newx,newy) == null || brd.getpiece(newx,newy).color != color)
+			canMove = true;
+		return canMove;
+		
+	}
+
 	
 }
