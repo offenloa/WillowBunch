4977b2997c7a604fb26bc4684815cd2419e63b9f
Owen <owenburland@live.ca>
2021-01-30 11:52:05 -0600

    added bishop logic

--- Bishop.java
+++ Bishop.java
@@ -1,6 +1,9 @@
 import java.awt.Toolkit;
-
+import java.math.*;
 public class Bishop extends Piece {
+
+	int highX,highY,lowX,lowY;
+	boolean canMove;
 	
 	public Bishop(int x, int y, boolean color, Board brd) {
 		super(x,y,color,brd);
@@ -10,5 +13,24 @@ public class Bishop extends Piece {
 			super.setSprite(Toolkit.getDefaultToolkit().getImage("Chess_Pieces/Chess_bdt60.png"));
 			
 	}
+
+	public boolean move(int newx, int newy){
+
+		highX = Math.max(newx, x);
+		highY = Math.min(newy, y);
+		lowX = Math.max(newx, x);
+		lowY = Math.min(newy, y);
+
+		for(int i = 0; i < newx - x;i++){
+			if(brd.getpiece(lowX+i, lowY+i) != null)
+				canMove = false;
+		}
+		if(brd.getpiece(highX, highY).color == color)
+			canMove = false;
+
+		return canMove;
+	}
+
+	
 	
 }
