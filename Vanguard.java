8070a250454d656e585bc8a53d42fd7055b292c9
Owen <owenburland@live.ca>
2021-01-30 15:17:17 -0600

    added vanguard art

--- Vanguard.java
+++ Vanguard.java
@@ -1,3 +1,13 @@
-public class Vanguard {
+import java.awt.Toolkit;
+
+public class Vanguard extends Piece{
     
+    public Vanguard(int x, int y, boolean color, Board brd) {
+		super(x,y,color,brd);
+		if(color)
+			super.setSprite(Toolkit.getDefaultToolkit().getImage("Chess_Pieces/Chess_vlt60.png"));
+		else
+			super.setSprite(Toolkit.getDefaultToolkit().getImage("Chess_Pieces/Chess_vdt60.png"));
+			
+	}
 }
