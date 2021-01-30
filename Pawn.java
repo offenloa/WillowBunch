5ac43d01df1ec93e12d8df19335f68d54a3fb59a
Owen <owenburland@live.ca>
2021-01-30 12:12:42 -0600

    added logic to pawn

--- Pawn.java
+++ Pawn.java
@@ -1,6 +1,7 @@
 import java.awt.Toolkit;
 
 public class Pawn extends Piece {
+	boolean canMove;
 	
 	public Pawn(int x, int y, boolean color, Board brd) {
 		super(x,y,color,brd);
@@ -9,6 +10,17 @@ public class Pawn extends Piece {
 		else
 			super.setSprite(Toolkit.getDefaultToolkit().getImage("Chess_Pieces/Chess_pdt60.png"));
 			
+	}
+
+	public boolean move(int newx, int newy){
+		canMove = true;
+		if(newx != x)
+			canMove = false;
+		for(int i = y; i<=newy;i++){
+			if(brd.getpiece(x,i) != null)
+				canMove = false;
+		}
+		return canMove;
 	}
 	
 }
